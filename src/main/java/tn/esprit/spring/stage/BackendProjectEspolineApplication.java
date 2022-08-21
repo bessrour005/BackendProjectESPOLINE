552766db837c3b4a72da9f47ea0a3553dd32ee7a
BessrourAsma <asma.bessrour@esprit.tn>
2022-08-22 01:59:39 +0200

    initial commit

---
+++ src/main/java/tn/esprit/spring/stage/BackendProjectEspolineApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectEspolineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectEspolineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/spring/stage/BackendProjectEspolineApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectEspolineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
